b8cd83f1a04941c808a9848df10d021eb453f624
mark041002 <132442951+mark041002@users.noreply.github.com>
2024-06-28 22:31:48 +0000

    cors hinzugefügt

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,13 @@
 import express from 'express';
-
+import cors from 'cors';
 /** Zentrales Objekt für unsere Express-Applikation */
 const app = express();
+app.use(express.json());  
+app.use(cors());
+/*app.use((req, res, next) => {
+    req.db = todo_database;
+    next();
+});*/
 
 /**
  * Liste aller ToDos. 
@@ -28,8 +34,51 @@ let TODOS = [
     }
 ];
 
-// Your code here
-app.get('/todos', (req, res) => {
+// Lesen aller ToDos (GET)
+app.get('/api/todos', (req, res) => { //Ouput: Kompletter Datensatz
     res.json(TODOS);
 });
+
+// Lesen eines einzelnen ToDos (GET)
+app.get('/api/todos/:id', (req, res) => { //Output: Einzelner Datensatz
+    const id = parseInt(req.params.id);
+    const todo = TODOS.find(todo => todo._id === id);
+    if (todo) {
+        res.json(todo);
+    } else {
+        res.status(404).send();
+    }
+});
+
+// Erstellen eines neuen ToDos (POST)
+app.post('/api/todos', (req, res) => {  //Input: Neuer Datensatz
+    const newTodo = req.body;
+    TODOS.push(newTodo);
+    res.status(201).json(newTodo);
+});
+
+// Aktualisieren eines ToDos (PUT)
+app.put('/api/todos/:id', (req, res) => { // Update eines Datensatzes
+    const id = parseInt(req.params.id);
+    const updatedTodo = req.body;
+    const index = TODOS.findIndex(todo => todo._id === id);
+    if (index !== -1) {
+        TODOS[index] = updatedTodo;
+    } else {
+        res.status(404).send();
+    }
+});
+
+// Löschen eines ToDos (DELETE)
+app.delete('/api/todos/:id', (req, res) => { //Löschen eines Datensatzes
+    const id = parseInt(req.params.id);
+    const index = TODOS.findIndex(todo => todo._id === id);
+    if (index !== -1) {
+        TODOS.splice(index, 1);
+        res.status(204).send();
+    } else {
+        res.status(404).send();
+    }
+});
+
 app.listen(3000);
